08967618fb1ccb3ed5d6a28e1d4d3a0de8eea5d4
wuxutao <wuxutao@mafengwo.cn>
2020-03-25 08:58:30 +0800

    打包

--- main.go
+++ main.go
@@ -6,11 +6,21 @@ import (
 
 	"only/src/userinterface/api/admin"
 
+	"github.com/rs/cors"
+
 	"github.com/kataras/iris"
 )
 
 func main() {
 	app := iris.Default()
+	corsOptions := cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowCredentials: true,
+	}
+
+	corsWrapper := cors.New(corsOptions).ServeHTTP
+
+	app.WrapRouter(corsWrapper)
 
 	app.Handle("GET", "/ping", func(ctx iris.Context) {
 		ctx.JSON(iris.Map{"message": "ponga"})
@@ -27,4 +37,15 @@ func main() {
 	// Listens and serves incoming http requests
 	// on http://localhost:8080.
 	app.Run(iris.Addr(":9999"))
+}
+
+func Cors(ctx iris.Context) {
+	ctx.Header("Access-Control-Allow-Origin", "*")
+	if ctx.Request().Method == "OPTIONS" {
+		ctx.Header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,OPTIONS")
+		ctx.Header("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization")
+		ctx.StatusCode(204)
+		return
+	}
+	ctx.Next()
 }
